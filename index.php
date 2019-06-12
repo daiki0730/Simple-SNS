453287cb88de99720ec0d6c8e3fa32de590c757e
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-06-12 21:28:30 +0900

    検索用フォームを追加
    投稿内容を検索するため

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 <title>SNS</title>
 </head>
 <body>
+
 <form action="complete.php" method="post">
 名前
 <div><input type="text" name="name"></div>
@@ -12,6 +13,15 @@
 <div><textarea name="text"></textarea></div>
 <input type="submit" value="send">
 </form>
+
+<hr>
+<form action="search.php" method="post">
+検索キーワード
+<div><input type="text" name="search"></div>
+<input type="submit" value="search">
+</form>
+<hr>
+
 <?php
 $db = new PDO("mysql:host=localhost;dbname=db","root","root");
 $ps = $db->query("SELECT * FROM tb ORDER BY num DESC");
