5a8d2b5574c7cf26debb07873fd62ac26b225328
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-06-12 21:04:27 +0900

    first comit

---
+++ complete.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>SNS</title>
+</head>
+<body>
+<?php
+$my_name = htmlspecialchars($_POST["name"],ENT_QUOTES);
+$my_text = htmlspecialchars($_POST["text"],ENT_QUOTES);
+$db = new PDO("mysql:host=localhost;dbname=db","root","root");
+$db->query("INSERT INTO tb (num,name,text,time)
+            VALUES (NULL,'$my_name','$my_text',NOW())");
+echo "Post complete";
+echo "<p><a href='index.php'>一覧表示へ</a></p>";
+?> 
+</body>っっっっっｃ
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>SNS</title>
+</head>
+<body>
+<form action="complete.php" method="post">
+名前
+<div><input type="text" name="name"></div>
+メッセージ
+<div><textarea name="text"></textarea></div>
+<input type="submit" value="send">
+</form>
+<?php
+$db = new PDO("mysql:host=localhost;dbname=db","root","root");
+$ps = $db->query("SELECT * FROM tb ORDER BY num DESC");
+while ($r = $ps->fetch()) {
+echo "{$r['num']} {$r['name']} {$r['time']}<br>"
+      .nl2br($r['text'])."<hr>";
+}
+?> 
+</body>
